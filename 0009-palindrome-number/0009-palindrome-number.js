6d5a863ded1ec89bfd5ff477e53818dbf4f75e12
Arslan <100716135+Arslan909@users.noreply.github.com>
2024-03-13 00:27:29 +0500

    Time: 124 ms (84.33%), Space: 57.6 MB (48.75%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -5,9 +5,8 @@
 var isPalindrome = function (x) {
     if (x < 0) 
         return false;
-    if (x === 0) 
-        return true;
-
+    if(x === 0)
+        return true; // bcoz i dont want to change temp -ignore this for sanity
     let temp = '';
     let org = x;
     while (x !== 0) {
