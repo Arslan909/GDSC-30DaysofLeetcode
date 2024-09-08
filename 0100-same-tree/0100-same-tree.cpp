a5b5596ad548fba4c730abbe575701f3c2cc2ef1
Arslan <100716135+Arslan909@users.noreply.github.com>
2024-09-08 23:52:18 +0500

    Time: 0 ms (100%), Space: 12.2 MB (6.21%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -6,22 +6,26 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (p == nullptr && q == nullptr){
+        if (p == nullptr && q == nullptr) {
             return true;
         }
-        if (p == nullptr || q == nullptr){
+        if (p == nullptr || q == nullptr) {
             return false;
         }
-        if (p->val != q->val){
-            return false;
-        }
-        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
 
+        if (p->val == q->val) {
+            if (isSameTree(p->left, q->left) &&
+                isSameTree(p->right, q->right)) {
+                return true;
+            }
+        }
+        return false;
     }
 };
